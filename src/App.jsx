15da5ce1b9d63540f554a1bf340539ecc770a4f1
nicoweiss1 <148877618+nicoweiss1@users.noreply.github.com>
2024-06-21 10:05:38 +0200

    First commit
    Login without User data

--- src/App.jsx
+++ src/App.jsx
@@ -1,108 +1,31 @@
-import React from "react";
-import "./App.css";
-import Footer from "./Footer";
-import Header from "./Header";
+import React from 'react';
+import './App.css';
 
-const trips = [
-  {
-    id: 1,
-    title: "BT01",
-    description: "San Francisco World Trade Center on new Server/IOT/Client ",
-    startTrip: [2021, 2, 13, 0, 0],
-    endTrip: [2021, 2, 15, 16, 56],
-    meetings: [
-      {
-        id: 1,
-        title: "One Conference",
-        description: "Key Note on One Conference",
-      },
-      {
-        id: 2,
-        title: "Zero Conference",
-        description: "Workshop Zero on One Conference",
-      },
-    ],
-  },
-  {
-    id: 2,
-    title: "BT02",
-    description: "Santa Clara Halley on new Server/IOT/Client",
-    startTrip: [2021, 6, 23, 9, 0],
-    endTrip: [2021, 6, 27, 16, 56],
-    meetings: [
-      {
-        id: 3,
-        title: "One Conference",
-        description: "HandsOn on One Conference",
-      },
-      {
-        id: 4,
-        title: "One Conference",
-        description: "Key Note on One Conference",
-      },
-    ],
-  },
-  {
-    id: 3,
-    title: "BT03",
-    description: "San Cose City Halley on Docker/IOT/Client",
-    startTrip: [2021, 12, 13, 9, 0],
-    endTrip: [2021, 12, 15, 16, 56],
-    meetings: [
-      {
-        id: 5,
-        title: "One Conference",
-        description: "Key Note on One Conference",
-      },
-    ],
-  },
-];
-
-export default function App() {
-  function renderTrip(t) {
-    return (
-      <div className="product" key={t.id}>
-        <figure>
-          <div>
-            <img src={"images/items/" + t.id + ".jpg"} alt="name " />
-          </div>
-          <figcaption>
-            <a href="#. . . ">{t.title}</a>
-            <div>
-              <span>
-                {t.startTrip[2] + "-" + t.startTrip[1] + "-" + t.startTrip[0]}
-              </span>
-            </div>
-            <p>{t.description}</p>
-            <div>
-              <button type="button" disabled>
-                Add to Triplist
-              </button>
-            </div>
-          </figcaption>
-        </figure>
-      </div>
-    );
-  }
-
+function App() {
   return (
-    <>
-      <div>
-        <Header />
-        <main>
-          <section id="filters">
-            <label htmlFor="month">Filter by Month:</label>{" "}
-            <select id="size">
-              <option value="">All months</option>
-              <option value="1">January</option>
-              <option value="2">February</option>
-              <option value="3">March</option>
-            </select>
-          </section>
-          <section id="products">{trips.map(renderTrip)}</section>
-        </main>
-      </div>
-      <Footer />
-    </>
+    <div className="App">
+      <header className="App-header">
+        <Login />
+      </header>
+    </div>
   );
 }
+
+function Login() {
+  return (
+    <div className="login-container">
+      <h2>Member Login</h2>
+      <form>
+        <div className="input-container">
+          <input type="text" placeholder="Username" />
+        </div>
+        <div className="input-container">
+          <input type="password" placeholder="Password" />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
